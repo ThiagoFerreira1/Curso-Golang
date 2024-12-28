9cbe63bf558762a38befb68d73255f1ca91f72ef
Thiago Alves Ferreira <thiagoalvesferreira.taf@gmail.com>
2024-12-27 23:57:02 -0300

    Primeiro projeto

---
+++ fundamentos/primeiro/hello.go
@@ -0,0 +1,14 @@
+// Programas executáveis iniciam pelo pacote main
+package main
+
+/*
+Os códigos em GO são orgnizados em pacotes
+e para utilizá-los é necessário declarar um ou vários imports
+*/
+import "fmt"
+
+// A porta de entrada de um pograma em GO é a função main()
+func main() {
+	fmt.Print("Hello ")
+	fmt.Print("World! ")
+}
